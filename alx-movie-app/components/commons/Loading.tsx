8860401e2b81de7035896e649e342bf0f956ccee
Vx-tech <xaviervianney8@gmail.com>
2025-12-07 17:01:06 +0300

    Movie page creation

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -1,11 +1,14 @@
-const Loading: React.FC<Loading> = ({ imageUrl, prompt, width, action }) => {
+const Loading: React.FC = () => {
   return (
-    <div onClick={() => action(imageUrl)} className="mt-6 border hover:cursor-pointer">
-      <img src={imageUrl} alt={prompt} className={`w-full max-w-md rounded-lg shadow-lg`} />
-      <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Your Prompt:</h2>
-      <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-700 mb-4`}>{prompt}</p>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-6xl font-semibold text-white mb-4 animate-pulse">
+          Loading...
+        </h1>
+        <p className="text-lg text-gray-300">Please wait, we&apos;re getting next set of movies ready for you.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Loading;
--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,11 +1,19 @@
-const MovieCard: React.FC<MovieCard> = ({ imageUrl, prompt, width, action }) => {
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
+
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
   return (
-    <div onClick={() => action(imageUrl)} className="mt-6 border hover:cursor-pointer">
-      <img src={imageUrl} alt={prompt} className={`w-full max-w-md rounded-lg shadow-lg`} />
-      <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Yours Prompt:</h2>
-      <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-700 mb-4`}>{prompt}</p>
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
     </div>
   )
 }
 
-export default MovieCard;
+export default MovieCard
